94a456cd4abcf17d525023fb2f302081c77996a7
Chad Lengefeld <chadlengefeld@gmail.com>
2019-11-02 19:13:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -88,6 +88,7 @@ def adding_matz
 		 programmer_hash[user]
 		 yukihiro_matsumoto = [:known_for => "Ruby"] [:languages => ["LISP", "C"]]
 		 programmer_hash[user] = [:yukihiro_matsumoto]
+		 programmer_hash.merge(programmer_hash[user])
 			#programmer_hash[:first] = "yukihiro_matsumoto"
 	#		programmer_hash[:yukihiro_matsumoto] = :known_for
 		#	programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
