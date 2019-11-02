2aa67e042698ee2488ddb99efcf48a257af8bee6
Chad Lengefeld <chadlengefeld@gmail.com>
2019-11-02 18:57:21 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,7 +85,7 @@ def adding_matz
         }
      }
 
-			[programmer_hash][:first] = "yukihiro_matsumoto"
+			programmer_hash[:first] = "yukihiro_matsumoto"
 	#		programmer_hash[:yukihiro_matsumoto] = :known_for
 		#	programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
 		#known_for = yukihiro_matsumoto[:known_for]
